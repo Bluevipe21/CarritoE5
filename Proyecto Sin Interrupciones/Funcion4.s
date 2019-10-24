dd7a5a219864012512b0e78c2fccb0e0dece8703
Bluevipe21 <bluevipe21@gmail.com>
2019-10-23 18:40:47 -0600

    Add files via upload

--- Proyecto Sin Interrupciones/Funcion4.s
+++ Proyecto Sin Interrupciones/Funcion4.s
@@ -9,7 +9,7 @@ PA6       EQU 0x40004100
 PA7       EQU 0x40004200
 FIVESEC    EQU 26666667      ; approximately 5s delay at ~16 MHz clock
 THREESEC	EQU 16000000
-TWOSEC EQU 10666667
+TWOSEC EQU 5000000
 ONESEC EQU 2666667 ;Tiempo para los giros de 90 grados
 ;Registros para puerto D
 LEDS               EQU 0x4000703C   ; Acceso a PD3-PD0
@@ -41,6 +41,223 @@ PA7PRESSED
 	LDR R0,=FIVESEC					;Asigna valor de retardo
 	BL delay						;Ejecuta el retardo
 	;Aqui comenzar la funcion
+	;Mover recto
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=6500000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=4200000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=3250000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=6300000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=8667221
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=5200000;Vuelta antes de la segunda hipotenusa
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=7667221 ;Segunda hipotenusa
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=5400000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=3800000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro ACA EMPIEZA EL CIRCULO
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2100000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1400000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2100000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1400000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2100000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1400000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0];ACA VAMOS
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2500000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1400000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2500000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1300000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2800000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1400000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2900000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Mover recto 
+	LDR R1,=(IN1+IN3)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=1200000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	;Giro 
+	LDR R1,=(IN1)
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]                    ; [R0] = R1 = 9 PD0 ENCIENDE - IN4
+	LDR R0,=2200000
+	BL delay
+	LDR R1,=APAGAR
+	LDR R0, =LEDS ;se asigna R0 como registro de control para los puertos D
+	STR R1, [R0]
+	
 	MOV R4,#0						;se asigna 0 al valor de salida de PF2
 	BL PortF_Output					;establece valor de salida de PF2
 	BL Loop			
